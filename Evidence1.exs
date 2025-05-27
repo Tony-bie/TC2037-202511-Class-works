b28e25aaab404f2778d4346fb7c2e284e7f85351
Antonio Pires <A01424547@tec.mx>
2025-05-26 18:44:37 -0600

    Evidence and style modified and improved

--- Evidence1.exs
+++ Evidence1.exs
@@ -7,68 +7,56 @@
 #Note: To run the code,put this in the terminal: "elixir Evidence1 File.cpp"
 defmodule CppLexer do
 
-
 #Defining regular expressions for different types of tokens with modules
-  @include_directive_regex ~r/^(#include|#ifndef)/
-  @class_regex ~r/^([A-Z][a-z_]*)/
-  @header_regex ~r/^(<[^>]+>|"[^"]+")/
-  @define_regex ~r/^(#define)/
-  @define_text_regex ~r/^([A-Z][A-Z0-9_]*)/
-  @comment_single_regex ~r/^(\/\/.*)/
-  @comment_multi_start_regex ~r/^(\/\*)/
-  @comment_multi_end_regex ~r/^(.*?\*\/)/
-  @std_regex ~r/^(std::)/
-  @vector_class_regex ~r/^(vector|[A-Z][a-z]*)/
-  @type_regex ~r/^(void|int|string|float|double|char|auto|bool|long|short|unsigned|signed|wchar_t|size_t)/
-  @keyword_regex ~r/^(for|while|do|if|else|switch|case|default|break|continue|return|new|delete|try|catch|throw|class|struct|template|namespace|using|typedef|virtual|friend|static|const|inline|explicit|operator|sizeof|private|public|protected|nullptr)/
-  @function_regex ~r/^([a-zA-Z_][a-zA-Z0-9_]*)\s*\(/
-  @identifier_regex ~r/^([a-zA-Z_][a-zA-Z0-9_]*)/
-  @number_regex ~r/^(0[xX][0-9a-fA-F]+|0[bB][01]+|0[0-7]+|\d+\.\d+([eE][+-]?\d+)?|\d+)/
-  @string_regex ~r/^"([^"\\]|\\.)*"/
-  @char_regex ~r/^'([^'\\]|\\.)*'/
-  @operator_regex ~r/^(\+\+|--|->|\+=|-=|\*=|\/=|%=|&=|\|=|\^=|<<=|>>=|<<|>>|==|!=|<=|>=|&&|\|\||::|<=>|[+\-*\/%<>=!&|^~.,])/
-  @punctuation_regex ~r/^([\[\]\(\)\{\};:])/
+  @comment_single_regex ~r/^\/\/.*/
+  @comment_multi_regex ~r/^\/\*.*?\*\//
+  @string_regex ~r/^"[^"]*"/
+  @char_regex ~r/^'[^']*'/
+  @number_regex ~r/^(\d+\.\d+|\d+)/
+  @preprocessor_regex ~r/^#\w+/
+  @header_regex ~r/^<\w+>/
+  @template_regex ~r/^template/
+  @keyword_regex ~r/^(class|struct|int|void|char|bool|float|double|string|for|while|if|else|return|new|delete|public|private|protected|static|const|virtual|namespace|using|typedef|auto|long|short|unsigned|signed)/
+  @std_regex ~r/^std::/
+  @scope_regex ~r/^::/
+  @class_name_regex ~r/^[A-Z]\w*/
+  @function_regex ~r/^[a-z_]\w*(?=\s*\()/
+  @identifier_regex ~r/^[a-zA-Z_]\w*/
+  @operator_regex ~r/^(\+\+|--|->|<<|>>|<=|>=|==|!=|&&|\|\||[+\-*\/%<>=!&|^~])/
+  @punctuation_regex ~r/^[(){}\[\];:,.<>]/
+  @space_regex ~r/^\s+/
 
-
-# Variables added to a module list with their corresponding token types,
+#Variables added to a module list with their corresponding token types,
 #next to it are your CSS tags, each token type is assigned a different color.
   @regex_list [
-    {@include_directive_regex, :preprocessor},
-    {@class_regex, :class},
-    {@header_regex, :string},
-    {@define_regex, :preprocessor},
-    {@define_text_regex, :constant},
     {@comment_single_regex, :comment},
-    {@comment_multi_start_regex, :comment},
-    {@comment_multi_end_regex, :comment},
-    {@std_regex, :namespace},
-    {@vector_class_regex, :container},
-    {@type_regex, :type},
-    {@keyword_regex, :keyword},
-    {@function_regex, :function},
-    {@identifier_regex, :identifier},
-    {@number_regex, :number},
+    {@comment_multi_regex, :comment},
     {@string_regex, :string},
     {@char_regex, :character},
+    {@number_regex, :number},
+    {@preprocessor_regex, :preprocessor},
+    {@header_regex, :header},
+    {@template_regex, :template_keyword},
+    {@keyword_regex, :keyword},
+    {@std_regex, :namespace},
+    {@scope_regex, :scope},
+    {@function_regex, :function},
+    {@class_name_regex, :class},
     {@operator_regex, :operator},
-    {@punctuation_regex, :punctuation}
+    {@punctuation_regex, :punctuation},
+    {@identifier_regex, :identifier},
+    {@space_regex, :space}
   ]
-
   #Main function that receives the name of the input file
   def analyze(input_file) do
-
     name = Path.rootname(input_file)
     html_file = "#{name}.html" #Create the html file name
-    css_file = "token_colors.css" #Create css file name
 
     content = File.read!(input_file) #Read the file
     tokens = tokenize(content) #Tokenize the file content
     html = generate_html(tokens) #Send the tokens to the function that generates the HTML
 
-    css = generate_css() #Generates CSS for syntax highlighting
-
     File.write!(html_file, html) #Write the HTML to a file
-    File.write!(css_file, css) #Write the CSS to a file
   end
 
   #Function that take the files's content to tokenize as paragraphs
@@ -101,8 +89,8 @@ defmodule CppLexer do
         {_,resto} = String.split_at(line, String.length(match))
         tokenize_line(resto, [{match, type} | list])
       nil ->
-        {char, resto} = String.split_at(line, 1)
-        tokenize_line(resto, [{char, :nil} | list])
+        {_, resto} = String.split_at(line, 1)
+        tokenize_line(resto, list)
     end
   end
 
@@ -112,7 +100,7 @@ defmodule CppLexer do
     <!Doctype html>
     <html>
     <head>
-      <link rel="stylesheet" href="token_colors.css">
+      <link rel="stylesheet" href="style.css">
     </head>
     <body>
       <pre>#{generate_spans(tokens)}</pre>
@@ -136,100 +124,6 @@ defmodule CppLexer do
       "<span class=\"#{type}\">#{escape_html(text)}</span>"
     end)
     |>Enum.join("")
-  end
-
-  #Generator of css
-  def generate_css do
-    """
-        body {
-        font-family: 'Courier New', monospace;
-        line-height: 1.5;
-        margin: 20px;
-        background-color: #f5f5f5;
-
-    }
-
-    pre {
-        background-color: #ffffff;
-        padding: 15px;
-        border-radius: 5px;
-        border: 1px solid #ddd;
-        overflow-x: auto;
-    }
-
-    /* Estilos para los diferentes tipos de token */
-    .keyword {
-        color: #0000ff;
-        font-weight: bold;
-    }
-
-    .preprocessor {
-        color: #008000;
-    }
-
-    .comment {
-        color: #008000;
-        font-style: italic;
-    }
-
-    .string {
-        color: #a31515;
-    }
-
-    .character {
-        color: #a31515;
-    }
-
-    .number {
-        color: #098658;
-    }
-
-    .identifier {
-        color: #000000;
-    }
-
-    .operator {
-        color: #800000;
-    }
-
-    .punctuation {
-        color: #000000;
-    }
-
-    .type {
-        color: #2b91af;
-        font-weight: bold;
-    }
-
-    .function {
-        color: #795e26;
-    }
-
-    .namespace {
-        color: #2b91af;
-    }
-
-    .container {
-        color: #2b91af;
-    }
-
-    .constant {
-        color: #0000ff;
-    }
-
-    .include {
-        color: #008000;
-    }
-
-    .define {
-        color: #008000;
-        font-weight: bold;
-    }
-
-    .class{
-      color: #c757ff
-    }
-    """
   end
 
   #Changer of character to avoid conflicts with the html
